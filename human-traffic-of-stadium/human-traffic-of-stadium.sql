e5d7100bde226d9531448fc1b2112b767694e159
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-05-11 19:27:02 +0530

    Time: 724 ms (36.92%) | Memory: 0B (100.00%) - LeetSync

---
+++ human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT id, visit_date, people
+FROM (
+  SELECT *, COUNT(*) OVER (PARTITION BY island) AS cnt
+  FROM (
+    SELECT *, id - ROW_NUMBER() OVER (ORDER BY id) AS island
+    FROM Stadium
+    WHERE people >= 100
+  ) AS subquery
+) AS subquery2
+WHERE cnt >= 3
+ORDER BY visit_date;
