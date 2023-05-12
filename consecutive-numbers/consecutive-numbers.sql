e72ec04a82a7fb1fb071b39a4ee7c6b43b017275
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-05-12 19:38:24 +0530

    Time: 774 ms (98.64%) | Memory: 0B (100.00%) - LeetSync

---
+++ consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT DISTINCT A.Num AS ConsecutiveNums
+FROM Logs AS A
+INNER JOIN Logs AS B ON A.Id + 1 = B.Id
+INNER JOIN Logs AS C ON B.Id + 1 = C.Id
+WHERE A.Num = B.Num
+    AND B.Num = C.Num
