afea76eb0defa1eebdfbea40d149b3a231a5fd3a
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-05-31 08:35:23 +0530

    Time: 34 ms (24.42%) | Memory: 51.2 MB (5.16%) - LeetSync

---
+++ zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        
+        vector<string> ans(numRows);
+
+        if(numRows==1)
+        {
+            return s;
+        } 
+
+        bool flag = false;
+        int i = 0;
+
+        for(auto ch : s)
+        {
+            ans[i] = ans[i] + ch;
+
+            if( i==0 || i==numRows-1)
+            {
+                flag = !flag;
+            }
+
+            if(flag)
+            {
+                i = i+1;
+            }
+            else
+            {
+                i = i-1;
+            }
+        }
+
+        string zigzag = "";
+
+        for(auto str : ans)
+        {
+            zigzag = zigzag+str;
+        }
+
+        return zigzag;
+    }
+};
