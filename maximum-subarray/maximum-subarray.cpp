4d2a29b71a91fb0d0b5a5f88a673b2dfec644eac
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-06-01 22:39:14 +0530

    Time: 115 ms (76.28%) | Memory: 67.6 MB (88.71%) - LeetSync

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -5,22 +5,23 @@ public:
         // step 1 : I have initialized two variables...
         int sum = 0;
         int maxSum = INT_MIN;
-
         //step 2:
-
         int n = nums.size();
         for(int i=0;i<n;i++)
         {
                 sum = sum + nums[i];
-
-                maxSum  = max(sum,maxSum);
+                // maxSum  = max(sum,maxSum);
+                // OR
+                if(maxSum < sum)
+                {
+                    maxSum = sum;
+                }
 
                 if(sum < 0)
                 {
                     sum = 0;
                 }
         } 
-
         return maxSum;
     }
 };
