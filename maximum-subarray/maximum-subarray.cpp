8f2b893ca02de63b86f6ffadecea3e017b28c32f
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-06-02 11:31:06 +0530

    Time: 136 ms (14.18%) | Memory: 67.7 MB (48.54%) - LeetSync

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -9,17 +9,17 @@ public:
         int n = nums.size();
         for(int i=0;i<n;i++)
         {
-                sum = sum + nums[i];
-                // maxSum  = max(sum,maxSum);
+                sum = sum + nums[i]; // 0 + (-1)  = -1
+                maxSum  = max(sum,maxSum); // -1  -infinity  --> //-1  
                 // OR
-                if(maxSum < sum)
-                {
-                    maxSum = sum;
-                }
+                // if(maxSum < sum)
+                // {
+                //     maxSum = sum;
+                // }
 
                 if(sum < 0)
                 {
-                    sum = 0;
+                    sum = 0;  //sum  = 0 
                 }
         } 
         return maxSum;
