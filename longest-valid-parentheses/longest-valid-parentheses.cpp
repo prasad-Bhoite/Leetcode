3ee655358d6ccad9e3977129d516f2b6fe4da3ce
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-06-03 08:17:16 +0530

    Time: 9 ms (21.91%) | Memory: 7.3 MB (39.12%) - LeetSync

---
+++ longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+
+        stack<int> st;
+        st.push(-1);
+
+        int len = 0;
+        int maxLen=0; 
+
+        for(int i=0;i<s.size();i++)
+        {
+            char ch = s[i];
+
+            if(ch == '(')
+            {
+                st.push(i);
+            }
+
+            else
+            {
+                st.pop();
+                if(!st.empty())
+                {
+                    int len = i-st.top();
+                    maxLen = max(len,maxLen);
+                }
+                else
+                {
+                    st.push(i);
+                }
+            }
+        }
+        return maxLen;      
+    }
+};
