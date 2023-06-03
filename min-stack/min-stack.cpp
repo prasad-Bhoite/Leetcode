3dfb7e0ee004bc9ccd3e2b91e911a7a6ae98558d
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-06-03 11:24:14 +0530

    Time: 21 ms (81.52%) | Memory: 16.4 MB (17.52%) - LeetSync

--- min-stack/min-stack.cpp
+++ min-stack/min-stack.cpp
@@ -1,47 +1,74 @@
+// class MinStack {
+// public:
+
+//     vector< pair<int,int> >  st;
+
+
+//     MinStack() { 
+//     }
+    
+//     void push(int val) {
+//        if(st.empty())
+//        {
+//            pair<int,int> p;
+//            p.first = val;
+//            p.second = val;
+//            st.push_back(p);
+//        }    
+//        else
+//        {
+//            pair<int,int> p;
+//            p.first = val;
+//            p.second = min(val,st.back().second);
+//            st.push_back(p);
+//        }
+//     }
+    
+//     void pop() {
+//         st.pop_back();
+//     }
+    
+//     int top() {
+//         return st.back().first;
+//     }
+    
+//     int getMin() {
+//         return st.back().second;
+//     }
+// };
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack* obj = new MinStack();
+//  * obj->push(val);
+//  * obj->pop();
+//  * int param_3 = obj->top();
+//  * int param_4 = obj->getMin();
+//  */
+
+
+
 class MinStack {
-public:
+ public:
+  void push(int x) {
+    if (stack.empty())
+      stack.emplace(x, x);
+    else
+      stack.emplace(x, min(x, stack.top().second));
+  }
 
-    vector< pair<int,int> >  st;
+  void pop() {
+    stack.pop();
+  }
 
+  int top() {
+    return stack.top().first;
+  }
 
-    MinStack() { 
-    }
-    
-    void push(int val) {
-       if(st.empty())
-       {
-           pair<int,int> p;
-           p.first = val;
-           p.second = val;
-           st.push_back(p);
-       }    
-       else
-       {
-           pair<int,int> p;
-           p.first = val;
-           p.second = min(val,st.back().second);
-           st.push_back(p);
-       }
-    }
-    
-    void pop() {
-        st.pop_back();
-    }
-    
-    int top() {
-        return st.back().first;
-    }
-    
-    int getMin() {
-        return st.back().second;
-    }
+  int getMin() {
+    return stack.top().second;
+  }
+
+ private:
+  stack<pair<int, int>> stack;  // {x, min}
 };
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
