837b83aa86c0a77f392b75605ec1ea9b0d389abb
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-06-03 11:36:21 +0530

    Time: 3 ms (84.09%) | Memory: 7.3 MB (39.12%) - LeetSync

--- longest-valid-parentheses/longest-valid-parentheses.cpp
+++ longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
-    int longestValidParentheses(string s) {
-
+    int longestValidParentheses(string s)  
+    {
+        //Step A : insert -1 in the stack...
         stack<int> st;
         st.push(-1);
 
         int len = 0;
-        int maxLen=0; 
+        int maxLen = 0; 
 
+        //Step B : insert open bracket...
         for(int i=0;i<s.size();i++)
         {
             char ch = s[i];
 
             if(ch == '(')
             {
+                //open bracket...
                 st.push(i);
             }
-
+            
             else
             {
+                //closed bracket ....
                 st.pop();
-                if(!st.empty())
-                {
-                    int len = i-st.top();
-                    maxLen = max(len,maxLen);
-                }
-                else
+                if(st.empty())
                 {
                     st.push(i);
                 }
+                else
+                {
+                    len = i - st.top();
+                    maxLen = max(maxLen,len);
+                }
             }
+
         }
-        return maxLen;      
+
+        return maxLen; 
+        
     }
 };
