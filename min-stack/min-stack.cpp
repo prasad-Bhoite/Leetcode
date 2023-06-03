b3786fe9b24f552d3c01d58529f64cc18ef18f93
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-06-03 11:23:37 +0530

    Time: 16 ms (96.76%) | Memory: 16.4 MB (42.58%) - LeetSync

--- min-stack/min-stack.cpp
+++ min-stack/min-stack.cpp
@@ -4,14 +4,15 @@ public:
     vector< pair<int,int> >  st;
 
 
-    MinStack() {
-        
+    MinStack() { 
     }
     
     void push(int val) {
        if(st.empty())
        {
-           pair<int,int> p = make_pair(val,val);
+           pair<int,int> p;
+           p.first = val;
+           p.second = val;
            st.push_back(p);
        }    
        else
