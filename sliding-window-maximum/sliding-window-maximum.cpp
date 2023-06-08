9d0c4ad633f7aa552042dea03072f8558ac9ad28
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-06-08 23:03:46 +0530

    Time: 297 ms (82.56%) | Memory: 134.6 MB (72.77%) - LeetSync

--- sliding-window-maximum/sliding-window-maximum.cpp
+++ sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,96 +1,96 @@
-// class Solution {
-// public:
-//     vector<int> maxSlidingWindow(vector<int>& nums, int k) 
-//     {
-//          deque<int> dq;
-//          vector<int> ans;
-
-//          // Step A: First Window traverse karna hai...
-//          for(int i=0;i<k;k++)
-//          {
-//              //chhote element remove kardo...
-//              while(!dq.empty() && nums[i] >= nums[dq.back()])
-//              {
-//                  dq.pop_back(); 
-//              }
-//             //Inserting index,so that we can checkout of window element...
-//              dq.push_back(i);
-//          }
-
-
-//          // Step B: store answer for first window ...
-//          ans.push_back(nums[dq.front()]);
-
-//          //Step C: Remaining Window ko process 
-//          for(int i=k;i<nums.size();i++)
-//          {
-//                 //out of window element ko remove kardia...
-//                 if(!dq.empty() && i-dq.front() >=k )
-//                 {
-//                     dq.pop_front();
-//                 }
-
-//                 //ab firse current element k liye chote element 
-//                 // ko remove karna hai...
-//                 //copy paste...
-//              while(!dq.empty() && nums[i] >= nums[dq.back()])
-//              {
-//                  dq.pop_back(); 
-//              }
-//              //Inserting index,so that we can checkout of window element...
-//              dq.push_back(i);
-
-//              //current window ka answer store karna hai...
-//              ans.push_back(nums[dq.front()]);
-//          }
-
-//          return ans;
-//     }
-// };
-
-
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
-        vector<int> ans;
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) 
+    {
+         deque<int> dq;
+         vector<int> ans;
 
-        //first window of k size
-        for(int i=0; i<k; i++){
-            //chote element remove krdo, 
-            while(!dq.empty() && nums[i] >= nums[dq.back()]) {
-                dq.pop_back();
-            }
-            //inserting index, so tht we can checkout of window element
-            dq.push_back(i);
-        }
+         // Step A: First Window traverse karna hai...
+         for(int i=0;i<k;i++)
+         {
+             //chhote element remove kardo...
+             while(!dq.empty() && nums[i] >= nums[dq.back()])
+             {
+                 dq.pop_back(); 
+             }
+            //Inserting index,so that we can checkout of window element...
+             dq.push_back(i);
+         }
 
-        //store answer for first window
-        ans.push_back(nums[dq.front()]);
 
-        //remaining windows ko process
-        for(int i=k; i<nums.size(); i++) {
-            //out of window element ko remove krdia 
-            if(!dq.empty() && i-dq.front() >= k) {
-                dq.pop_front();
-            }
+         // Step B: store answer for first window ...
+         ans.push_back(nums[dq.front()]);
 
-            //ab ferse current element k liyue chotte element 
-            //ko remove krna h 
-            while(!dq.empty() && nums[i] >= nums[dq.back()]) {
-                dq.pop_back();
-            }
-            //inserting index, so tht we can checkout of window element
-            dq.push_back(i);
+         //Step C: Remaining Window ko process 
+         for(int i=k;i<nums.size();i++)
+         {
+                //out of window element ko remove kardia...
+                if(!dq.empty() && i-dq.front() >=k )
+                {
+                    dq.pop_front();
+                }
 
-            //current window ka answer store krna h 
-            ans.push_back(nums[dq.front()]);
+                //ab firse current element k liye chote element 
+                // ko remove karna hai...
+                //copy paste...
+             while(!dq.empty() && nums[i] >= nums[dq.back()])
+             {
+                 dq.pop_back(); 
+             }
+             //Inserting index,so that we can checkout of window element...
+             dq.push_back(i);
 
-        }
+             //current window ka answer store karna hai...
+             ans.push_back(nums[dq.front()]);
+         }
 
-        return ans;
-    
-    
-    
+         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         deque<int> dq;
+//         vector<int> ans;
+
+//         //first window of k size
+//         for(int i=0; i<k; i++){
+//             //chote element remove krdo, 
+//             while(!dq.empty() && nums[i] >= nums[dq.back()]) {
+//                 dq.pop_back();
+//             }
+//             //inserting index, so tht we can checkout of window element
+//             dq.push_back(i);
+//         }
+
+//         //store answer for first window
+//         ans.push_back(nums[dq.front()]);
+
+//         //remaining windows ko process
+//         for(int i=k; i<nums.size(); i++) {
+//             //out of window element ko remove krdia 
+//             if(!dq.empty() && i-dq.front() >= k) {
+//                 dq.pop_front();
+//             }
+
+//             //ab ferse current element k liyue chotte element 
+//             //ko remove krna h 
+//             while(!dq.empty() && nums[i] >= nums[dq.back()]) {
+//                 dq.pop_back();
+//             }
+//             //inserting index, so tht we can checkout of window element
+//             dq.push_back(i);
+
+//             //current window ka answer store krna h 
+//             ans.push_back(nums[dq.front()]);
+
+//         }
+
+//         return ans;
+    
+    
+    
+//     }
+// };
