2cc14d67641fc4713d331abb8ef948e9b7656c59
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-06-10 22:51:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (22.21%) - LeetSync

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -53,19 +53,59 @@ public:
         return dp[n];
    }
 
+    int solveUsingTab(vector<int> &nums,int n)
+    {
+        ///Step 1 : Create DP array...
+        vector<int> dp(n+1,INT_MIN); //idhar dimag laga apun ko maximum chori chahiye to initially 0 ya INT_MIN store karle..
+
+        ///Step 2 : Handle the base cases in the form of dp[]...
+        //neglet the comparison wale cases...
+        //handle only assignment wale cases...
+        
+        //  if(n<0)
+        // {
+        //     return 0;
+        // }
+
+        //ek hi element hoga to utani hi chori hogi naa
+        
+            dp[0] = nums[0]; 
+
+        ///Step 3:  solve the logic behind it...
+        for(int i=1;i<=n;i++) // last element tak jana hai... and 0 ka kam ho gaya hai...
+        {
+            
+            int temp = 0;
+            if(i-2 >=0 )
+                temp = dp[i-2];
+
+            int include = temp + nums[i];
+            int exclude = dp[i-1] + 0;
+            dp[i]  = max(include,exclude);
+
+        }
+        return dp[n];
+    }
+
     int rob(vector<int>& nums) {
 
         //A : For Recursion ...
         // int n = nums.size()-1;
         // return solveUsingRecursion(nums,n);
 
-        //B : For Top Down Approach ...
 
+
+        //B : For Top Down Approach ...
         // Step 1 : Create DP array...
-        int n = nums.size()-1; //last index;
+        // int n = nums.size()-1; //last index;
         // u can put n+1  or  n+2 or n+20 but kam nahi honi chahiye...
-        vector<int> dp(n+1,-1);  
+        // vector<int> dp(n+1,-1);  
         // jiski value update ho rahi usko lele not a nums bcoz wo to same hai naa...
-        return solveUsingMem(nums,n,dp);
+        // return solveUsingMem(nums,n,dp);
+
+
+        // C : For Bottom Up Approach (Tabulation)
+        int n = nums.size()-1;
+        return solveUsingTab(nums,n);
     }
 };
