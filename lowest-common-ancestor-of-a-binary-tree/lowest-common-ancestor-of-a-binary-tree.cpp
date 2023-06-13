097dca5dee07d90257c41157486a6d56c0186c12
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-06-13 14:48:24 +0530

    Time: 24 ms (18.26%) | Memory: 14.2 MB (50.55%) - LeetSync

---
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    {
+        //base case...
+        if(root==NULL)
+        {
+            return NULL;
+        }   
+
+
+        //check p and q --> root node khud p or q to nahi hai checking...
+        if(root->val == p->val)
+        {
+            return p;
+        }
+
+        if(root->val ==q->val)
+        {
+            return q;
+        }
+
+        //kuch nahi mila to first check in the left and after that right...
+        TreeNode* leftAns = lowestCommonAncestor(root->left,p,q);
+        TreeNode* rightAns = lowestCommonAncestor(root->right,p,q);
+
+        //Logic 4 condition wala...
+        if(leftAns==NULL && rightAns==NULL)
+        {
+                return NULL;
+        }
+
+        else if(leftAns!=NULL && rightAns==NULL)
+        {
+            return leftAns;
+        }
+
+        else if(leftAns==NULL && rightAns!=NULL)
+        {
+            return rightAns;
+        }
+
+        else
+        {
+            return root;
+        }
+    }
+};
