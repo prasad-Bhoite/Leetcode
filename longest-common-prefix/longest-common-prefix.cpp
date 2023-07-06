d3f3af4928813e2c32d8bcaf881c6ed165fb1a4d
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-07-06 09:11:45 +0530

    Time: 4 ms (78.38%) | Memory: 9.3 MB (36.19%) - LeetSync

--- longest-common-prefix/longest-common-prefix.cpp
+++ longest-common-prefix/longest-common-prefix.cpp
@@ -4,47 +4,48 @@ class Solution {
     if (strs.empty())
       return "";
 
-    // for (int i = 0; i < strs[0].length(); ++i)
-    //   for (int j = 1; j < strs.size(); ++j)
-    //     if (i == strs[j].length() || strs[j][i] != strs[0][i])
-    //       return strs[0].substr(0, i);
+    //best approach ...
+    for (int i = 0; i < strs[0].length(); ++i)
+      for (int j = 1; j < strs.size(); ++j)
+        if (i == strs[j].length() || strs[j][i] != strs[0][i])
+          return strs[0].substr(0, i);
 
-    // return strs[0];
+    return strs[0];
 
     //M2 :Another way...
-    string ans;
-    int i=0;
+//     string ans;
+//     int i=0;
 
-    while(true)//infinite for loop
-    {
-      char curr_ch=0;
+//     while(true)//infinite for loop
+//     {
+//       char curr_ch=0;
 
-    for(auto str:strs)
-    {
-       //overflow...
-       if(i>=str.size())
-       {
-         curr_ch=0;
-         break;
-       }
-      //just started...
-      if(curr_ch==0)
-      {
-        curr_ch = str[i];
-      }
-      else if(curr_ch!=str[i])
-      {
-        curr_ch = 0;
-        break;
-      }
-    }
-    if(curr_ch==0)
-    {
-      break;
-    }
-    ans.push_back(curr_ch);
-    i++;
-  }
-  return ans;
+//     for(auto str:strs)
+//     {
+//        //overflow...
+//        if(i>=str.size())
+//        {
+//          curr_ch=0;
+//          break;
+//        }
+//       //just started...
+//       if(curr_ch==0)
+//       {
+//         curr_ch = str[i];
+//       }
+//       else if(curr_ch!=str[i])
+//       {
+//         curr_ch = 0;
+//         break;
+//       }
+//     }
+//     if(curr_ch==0)
+//     {
+//       break;
+//     }
+//     ans.push_back(curr_ch);
+//     i++;
+//   }
+//   return ans;
   }
 };
