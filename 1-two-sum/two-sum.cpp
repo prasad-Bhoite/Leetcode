4dbbaaba714eda8031a1c5ad8d332ded3480e08f
Prasad Bhoite <67198889+prasad-Bhoite@users.noreply.github.com>
2023-08-13 19:52:27 +0530

    Time: 3 ms (99.67%) | Memory: 10.3 MB (52.47%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        // int front=0;
+        // int rear = nums.size()-1;
+
+        // //sort the vector...
+        // sort(nums.begin(),nums.end());
+
+        // while(front < rear)
+        //   {
+        //       int sum = nums[front] + nums[rear];
+        //       if( sum == target )
+        //         {
+        //                 break;
+        //         }
+        //       else if(sum > target)
+        //        {
+        //            rear--;
+        //        }
+        //        else
+        //        {
+        //            front++;
+        //        }
+        //   }
+        //   return {front,rear};
+         
+         
+         
+         
+        int n=nums.size();
+        int a[n];
+        //3 2 4
+    //sorted arr 2 3 4 //l=0,r=2 check value at l and r and match with the copy of given array element 
+     for(int i=0;i<nums.size();i++){
+         a[i]=nums[i];
+     }
+        vector<int>v;
+        sort(nums.begin(),nums.end());
+               int l=0,r=nums.size()-1;
+        while(l<r){
+            if(nums[l]+nums[r]==target){
+              
+             break;
+            }
+            else if(nums[l]+nums[r]>target){
+                r--;
+            }
+            else{
+                l++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[l]==a[i]){
+                v.push_back(i);
+            }
+            else if(nums[r]==a[i]){
+                v.push_back(i);
+            }
+        }
+        
+        return v;
+    }
+};
